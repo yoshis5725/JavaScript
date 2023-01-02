494f1e882e947d0c94ea79b98499e46f34367439
elliottpolite <elliottpolite@gmail.com>
2023-01-02 06:30:38 -0700

    Completed Assignment

---
+++ app.js
@@ -0,0 +1,13 @@
+const firstName = "Elliott"
+let lastName = "Polite"
+var age = 39
+
+let fullName = `${firstName} ${lastName}`
+let city = "Atlanta"
+
+let pastime = "Watching cartoons while eating waffles"
+
+let myStory = `Hello! My name is ${fullName}. I am originally from ${city}. My favorite pastime would be ${pastime}.`
+
+console.log("fullName: ", fullName)
+console.log("My Story: ", myStory)
